c170b5d5c9c6ff805d4e3accc3ae5f4ba1e43432
Administrator <Administrator@192.168.12.51>
2019-08-28 17:56:36 +0200

    Fine mvc3

--- WebContent/js/s50.js
+++ WebContent/js/s50.js
@@ -14,5 +14,30 @@ $(function() {
 
     $('#swapColor').click(function() {
         $('#msg2').toggleClass('red');
-    });
+	    });
+	$('#pulsantered').click(function() {
+		let cur = $('#msg3');
+		cur.removeClass('yellow');
+		cur.addClass('red');
+	});
+
+	$('#pulsanteyellow').click(function() {
+		let cur = $('#msg3');
+
+		cur.removeClass('red');
+		cur.addClass('yellow');
+	});
+
+	$('swap3').click(function() {
+		let cur = $('msg3');
+		if (cur.hasClass('red')) {
+			cur.removeClass('red');
+			cur.addClass('yellow');
+		} else if (cur.hasClass('yellow')) {
+			cur.removeClass('yellow');
+
+		} else {
+			cur.addClass('red');
+		}
+	});
 });
--- WebContent/js/s51.js
+++ WebContent/js/s51.js
@@ -4,51 +4,86 @@
  * Slide 51
  */
 $(function() {
-    let important = false;
+	let important = false;
 
-    $('#changeMsg1').click(function() {
-        let msg = $('#msg1');
-        $('#target').html(msg.html());
-        if (important) {
-            msg.html('An <b>important</b> message');
-        } else {
-            msg.html('A <i>plain</i> message');
-        }
-        important = !important;
-    });
+	$('#changeMsg1').click(function() {
+		let msg = $('#msg1');
+		$('#target').html(msg.html());
+		if (important) {
+			msg.html('An <b>important</b> message');
+		} else {
+			msg.html('A <i>plain</i> message');
+		}
+		important = !important;
+			});
 
-    $('#changeTextMsg1').click(function() {
-        let msg = $('#msg1');
-        $('#target').text(msg.text());
-        if (important) {
-            msg.text('An <b>important</b> message');
-        } else {
-            msg.text('A <i>plain</i> message');
-        }
-        important = !important;
-    });
+	$('#changeTextMsg1').click(function() {
+		let msg = $('#msg1');
+		$('#target').text(msg.text());
+		if (important) {
+			msg.text('An <b>important</b> message');
+		} else {
+			msg.text('A <i>plain</i> message');
+		}
+		important = !important;
+	});
 
-    $('#changeMsg2').click(function() {
-        let msg = $('#msg2');
-        $('#target').text(msg.val());
-        if (important) {
-            msg.val('Important');
-        } else {
-            msg.val('Plain');
-        }
-        important = !important;
-    });
+	$('#changeMsg2').click(function() {
+		let msg = $('#msg2');
+		$('#target').text(msg.val());
+		if (important) {
+			msg.val('Important');
+		} else {
+			msg.val('Plain');
+		}
+		important = !important;
+	});
 
-    $('#upsize').click(function() {
-        let msg = $('#msg3');
-        let cur = parseInt(msg.css('font-size'));
-        msg.css('font-size', cur * 2);
-    });
+	$('#upsize').click(function() {
+		let msg = $('#msg3');
+		let cur = parseInt(msg.css('font-size'));
+		msg.css('font-size', cur * 2);
+	});
 
-    $('#downsize').click(function() {
-        let msg = $('#msg3');
-        let cur = parseInt(msg.css('font-size'));
-        msg.css('font-size', cur > 1 ? cur / 2 : 1);
-    });
-
+	$('#downsize').click(function() {
+		let msg = $('#msg3');
+		let cur = parseInt(msg.css('font-size'));
+		msg.css('font-size', cur > 1 ? cur / 2 : 1); //se cur>1 dimezzalo se no diventa 0, sparisce
+	});
+	
+	$("#random").click(function() {		
+	let result = Math.floor((Math.random()*6)+1);
+	$('#dado1').prop('src', './pic/' + result + '.jpg');
+	countRoller+=1;
+	
+ let n;
+	if(result>3){
+		n = $("<div>win</div>").css({color: 'green'}).appendTo('#result');
+	}else {
+		n = $("<div>lost</div>").css({color: 'red'}).appendTo('#result');
+	}
+	
 });
+	
+});
+
+//riga 17
+//oppure
+// if(important){
+// important=false;
+// }else
+// important=true;
+// } 
+
+//$("#random").click(function() {		
+//	let result = Math.floor((Math.random()*6)+1);
+//	$('#rn').val(result);
+//	
+// let n;
+//	if(result>3){
+//		n = $("<div>win</div>").css({color: 'green'}).appendTo('#result');
+//	}else {
+//		n = $("<div>lost</div>").css({color: 'red'}).appendTo('#result');
+//	}
+//	
+//});
